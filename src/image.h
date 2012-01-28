716bc6147b883647da0e38887a0a2f9a56d4de25
Matthias Richter <vrld@vrld.org>
2012-01-28 19:28:59 +0100

    Read only RGBA images. Header style!

--- src/image.h
+++ src/image.h
@@ -3,13 +3,9 @@
 
 struct lua_State;
 
-static const int BPP_LUMINANCE_ALPHA = 2;
-static const int BPP_RGBA = 4;
-
 typedef struct {
 	int width;
 	int height;
-	int bpp; // BYTES (!) per pixel
 	void* data;
 } image;
 
@@ -19,11 +15,6 @@ typedef struct {
 	unsigned char b;
 	unsigned char a;
 } rgba_pixel;
-
-typedef struct {
-	unsigned char l;
-	unsigned char a;
-} grey_pixel;
 
 image* l_checkimage(struct lua_State* L, int idx);
 int l_image_new(struct lua_State* L);
